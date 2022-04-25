8a5caf531c56560d6a98737797a7ae59ca1f008c
laszlo-ratesic <keenanrc@hotmail.com>
2022-04-25 11:20:55 -0500

    optimize images with webpack loaders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-require('bootstrap');
+require("bootstrap");
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
 
-$(document).ready(function() {
+$(document).ready(function () {
   // CREATEEL
-
-
 
   // SCHEDULE
 
   // EVENT
-
-  
 
   // TICKETS
 
@@ -18,21 +16,21 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
-      btnUrl: "schedule.html"
+      btnUrl: "schedule.html",
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
-      btnUrl: "tickets.html"
+      btnUrl: "tickets.html",
     },
-  ]
+  ];
 
   carouselSlides.forEach((slide, i) => {
-    $('.carousel-inner').append(`
+    $(".carousel-inner").append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
     <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
         <div class="container">
@@ -49,6 +47,6 @@ $(document).ready(function() {
           </div>
         </div>
     </div>
-  </div>`)
-  })
+  </div>`);
+  });
 });
